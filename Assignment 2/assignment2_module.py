fff077c2d74ca14766db65bd33445f7d8dff2ac9
shambhurajchavan721 <shambhuraj1144007@gmail.com>
2025-05-18 23:01:12 +0530

    Add files via upload

---
+++ Assignment 2/assignment2_module.py
@@ -0,0 +1,19 @@
+def Add(no1,no2):
+    ans = 0
+    ans = no1 + no2
+    return ans
+
+def Sub(no1,no2):
+    ans = 0
+    ans = no1 - no2
+    return ans
+
+def Mult(no1,no2):
+    ans = 0
+    ans = no1 * no2
+    return ans
+
+def Div(no1,no2):
+    ans = 0
+    ans = no1/no2
+    return ans
